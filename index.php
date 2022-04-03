8c2dd33a9a7e523df7b58f427b27c5c2814560c4
Alexandre Candeias <alexandreluisbarreto@gmail.com>
2022-04-03 19:37:07 +0100

    Adds a new command for "countdone"

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
 <?php
 
-include __DIR__ . '/vendor/autoload.php';
-include __DIR__ . '/helpers/main.php';
+include 'vendor/autoload.php';
 
+use App\Test;
+use App\Application;
 use Discord\Discord;
 use Discord\WebSockets\Event;
+use Discord\Parts\Embed\Embed;
+use Discord\Parts\Guild\Guild;
 use Discord\Parts\User\Member;
 use Discord\WebSockets\Intents;
 use Discord\Parts\Channel\Message;
+use Discord\Builders\MessageBuilder;
 use Discord\Parts\WebSockets\MessageReaction;
+
 
 /**
  * @see https://discord.com/developers/docs/intro
  */
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
+
+/* Application::run([
+    'token' => env('DISCORD_TOKEN'),
+    #'endpoint' => env('DISCORD_ENDPOINT'),
+    'logger' => true,
+    'intents' => Intents::GUILDS | Intents::GUILD_MESSAGES | Intents::GUILD_MESSAGE_REACTIONS,
+]);
+
+return; */
 
 $discord = new Discord([
     'token' => env('DISCORD_TOKEN'),
@@ -35,10 +49,16 @@ $allowedUsernames = [
 $discord->on(
     'ready',
     function (Discord $discord) use ($allowedUsernames) {
+
+        /* Test::run($discord);
+        return; */
+
+        /* $discord->guilds->get('599261105453268992'); */
+
         $log = $discord->getLogger();
         $log->info('Bot is ready!');
 
-        $discord->on(Event::MESSAGE_CREATE, function (Message $message) use ($log, $allowedUsernames) {
+        $discord->on(Event::MESSAGE_CREATE, function (Message $message) use ($log, $allowedUsernames, $discord) {
             $log->info('Message created!');
             $log->info('Username: ' . $message->author->username);
             $log->info('Channel: ' . $message->channel->name);
@@ -50,6 +70,7 @@ $discord->on(
                 return;
             }
 
+            // Commands
             if ($message->content === '/clear' && env('DISCORD_COMMAND_CLEAR', false)) {
                 $log->info('Clearing channel...');
                 $channel = $message->channel;
@@ -61,12 +82,41 @@ $discord->on(
                 return;
             }
 
+            // TODO: Add command to get all of the reports in the last:
+            // - 24 hours
+            // - 7 days
+            // - 30 days
+            // - On a specific server with the previous options (24 hours, 7 days & 30 days)
+
+            if ($message->content === '/countdone' && env('DISCORD_COMMAND_COUNT_DONE', false)) {
+                $log->info('Showing message of "CountDone" command...');
+
+                $message->delete()
+                    ->done(
+                        function () use ($message, $discord) {
+                            $message->channel->sendEmbed(
+                                (new Embed($discord))
+                                    ->setTitle('⚠️ CS:GO report count done.')
+                                    ->setColor(0xFFC300)
+                            );
+                        }
+                    );
+
+                return;
+            }
+
+            // END Commands
+
             if (!in_array($message->author->username, $allowedUsernames) && env('APP_ENV', 'prd') === 'prd') {
                 $log->info('User not allowed! <' . $message->author->username . '>');
                 return;
             }
 
-            $message->react('✅');
+            $message->react('✅')->done(
+                function () {
+                    // TODO : Add message id to the BD
+                }
+            );
 
             $message->createReactionCollector(
                 fn (MessageReaction $reaction) => $reaction->emoji->name === '✅',
@@ -80,7 +130,7 @@ $discord->on(
                          */
                         $user = $reactions->last()->user;
                         $message->reply('This report has been claimed by ' . $user . '!');
-                        // TODO: Send count +1 to the api to update the count for the mod
+                        // TODO: Add value of the user to the "claimed by" column in the BD
                     } else {
                         // Report expired
                         $message->deleteReaction(Message::REACT_DELETE_EMOJI, '✅');
