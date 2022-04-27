76e22a93e58f06f11e82bc6783b5a57643aa6148
alexandre433 <alexandreluisbarreto@gmail.com>
2022-04-28 00:39:44 +0100

    Updates MessageCreate class

--- app/events/MessageCreate.php
+++ app/events/MessageCreate.php
@@ -5,26 +5,49 @@ declare(strict_types=1);
 namespace App\Events;
 
 use App\EventAbstract;
+use Discord\Discord;
 use Discord\Parts\Channel\Message;
+use Psr\Log\LoggerInterface;
 
 class MessageCreate extends EventAbstract
 {
-    protected ?array $nonTerminatableFunctions = [
-        'test'
-    ];
+    /**
+     * Logger instance
+     *
+     * @var Psr\Log\LoggerInterface
+     */
+    protected LoggerInterface $log;
 
-    protected ?array $terminatableFunctions = [];
-
-    public function __construct()
+    /**
+     * Setup functions
+     * The name of this function is not required to be `setup`
+     *  the only function names you cannot use are the ones inside EventAbstract::class
+     *
+     * @param Message $message Message event object
+     * @param Discord $discord Discord class
+     *
+     * @return void
+     */
+    public function setUp(Message $message, Discord $discord)
     {
-        
-        echo 'asd' . PHP_EOL;
+        $this->log = $discord->getLogger();
+
+        $this->log->info('Log stuff');
     }
 
-    public function test(Message $message = null)
+    /**
+     * In this function, the event will terminate IF the function returns true
+     * if it doesn't return true, it will just keep going until there's no more functions in the class.
+     *
+     * @param Message $message Message event object
+     * @param Discord $discord Discord class
+     *
+     * @return true
+     */
+    public function terminateExample($message, $discord)
     {
-        #syslog(LOG_INFO, 'Message: ' . $message->content);
-        echo 'testing' . PHP_EOL;
-        #echo $message->content . PHP_EOL;
+        $this->log->info('another example');
+
+        return true;
     }
 }
